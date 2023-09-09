4098b9806028616ba47bb7598ed9bd5a43f89ca7
Andrey Lutcenko <andrey.lutcenko90@mail.ru>
2023-09-09 17:39:55 +0500

    feat(Converter): Добавлена метод создания и метод конвертации файлов в mp3

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
+import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+import config from "config";
+import { ogg } from "./ogg.js";
 
+const bot = new Telegraf(config.get("TELEGRAM_TOKEN"));
+
+bot.on(message("voice"), async (context) => {
+  try {
+    const link = await context.telegram.getFileLink(
+      context.message.voice.file_id
+    );
+    const userId = String(context.message.from.id);
+    const oggPath = await ogg.create(link.href, userId);
+    const mp3Path = await ogg.toMp3(oggPath, userId);
+    await context.reply(mp3Path);
+  } catch (error) {
+    console.log("Error while voice message", error.message);
+  }
+});
+
+bot.command("start", async (context) => {
+  await context.reply(JSON.stringify(context.message, null, 2));
+});
+
+bot.launch();
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
